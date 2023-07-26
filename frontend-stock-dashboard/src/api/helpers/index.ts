2582347a8f3333fd1293102184e6c2e68364a64a
Bhaskar Mishra <techbjunks@gmail.com>
2023-07-26 15:12:00 +0530

    Auto-refresh-logic

--- frontend-stock-dashboard/src/api/helpers/index.ts
+++ frontend-stock-dashboard/src/api/helpers/index.ts
@@ -1,22 +1,23 @@
-export const cachedApiCall = (time: number) => {
-    const cache: Record<string, { value: unknown; expiry: number }> = {};
-    
-    return async (url: string, config: RequestInit = {}) => {
-      const key = `${url}${JSON.stringify(config)}`;
-  
-      const entry = cache[key];
-      if (!entry || Date.now() > entry.expiry) {
-        try {
-          let resp = await fetch(url, config);
-          resp = await resp.json();
-          cache[key] = { value: resp, expiry: Date.now() + time };
-        } catch (e) {
-          console.log("error while making api call", e);
-        }
+import { StockDetail } from "../../common/types/stock";
+
+export const cachedApiCall = async () => {
+  const cache: Record<string, { value: unknown; expiry: number }> = {};
+
+  return async (url: string, config: RequestInit = {}, time = 0):Promise<StockDetail> => {
+    const key = `${url}${JSON.stringify(config)}`;
+    const entry = cache[key];
+    if (!entry || Date.now() > entry.expiry) {
+      try {
+        let resp = await fetch(url, config);
+        resp = await resp.json();
+        cache[key] = { value: resp, expiry: Date.now() + time };
+      } catch (e) {
+        console.log("error while making api call", e);
       }
-      return cache[key].value;
-    };
+    }
+
+    return cache[key].value;
   };
-  
-  export default cachedApiCall;
-  
+};
+
+export default cachedApiCall;
--- frontend-stock-dashboard/src/views/Details/Details.tsx
+++ frontend-stock-dashboard/src/views/Details/Details.tsx
@@ -9,6 +9,8 @@ import ErrorBoundary from "../../common/ui/ErrorBoundary";
 import SelectComponent from "../../common/components/Select/Select";
 import fetchStockDetailReducer from "../../api/reducer/stockdetails";
 
+const CLEAR = "clear";
+let intervalTimer: number | undefined = undefined;
 
 const Home = () => {
   const { symbol } = useParams();
@@ -19,30 +21,51 @@ const Home = () => {
 
   useEffect(() => {
     fetchStockDetail(symbol, dispatch);
+    return () => {
+      const controller = new AbortController();
+      controller.abort();
+    };
   }, [symbol]);
 
-  const updateAPIInterval = (value: [] | string):void => {
-    console.log('value', value);
-  }
+  const updateAPIInterval = (value: string | []): void => {
+    const numericValue = +value;
+
+    if (value === CLEAR) {
+      clearInterval(intervalTimer);
+      intervalTimer = undefined;
+    }
+
+    if (numericValue > 0) {
+      intervalTimer = setInterval(() => {
+        fetchStockDetail(symbol, dispatch, numericValue);
+      }, numericValue);
+    }
+  };
 
   return (
     <ErrorBoundary>
       <Header title="Stock Detail" />
-      <SelectComponent options={[
-        {
-          label: '5 min',
-          value: '5'
-        },
-        {
-          label: '10 min',
-          value: '10'
-        }
-      ]}
-      onChange={updateAPIInterval}
+      <SelectComponent
+        options={[
+          {
+            label: "1 min",
+            value: "3000",
+          },
+          {
+            label: "2 min",
+            value: "120000",
+          },
+          {
+            label: CLEAR,
+            value: CLEAR,
+          },
+        ]}
+        placeholder="Auto Refresh Time"
+        onChange={updateAPIInterval}
       />
       <StockTable isLoading={loading} data={data} key={data.Symbol} />
     </ErrorBoundary>
-  )
+  );
 };
 
 export default Home;
